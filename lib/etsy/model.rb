1403ae68d5fee516eb26b804ff49ddd187fb3354
Korakot Leemakdej <kleemakdej@gmail.com>
2021-04-12 11:57:16 -0700

    delay to fix Etsy request limit

--- lib/etsy/model.rb
+++ lib/etsy/model.rb
@@ -50,6 +50,8 @@ module Etsy
           num_batches = limit / batch_size
 
           num_batches.times do |batch|
+            # wait 0.5 second to delay Etsy request
+            sleep(0.5)
             total_offset = initial_offset + batch * batch_size
             response = Request.get(endpoint, options.merge(:limit => batch_size, :offset => total_offset))
             result << response.result
